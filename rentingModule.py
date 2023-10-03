ecb9d6cd689d05b3e7368fcfa96c7c4ea8fb160b
hunaidisar <37739827+hunaidisar@users.noreply.github.com>
2023-10-03 14:44:17 +0300

    Add files via upload

---
+++ rentingModule.py
@@ -0,0 +1,48 @@
+
+stock = {
+	"nissan" : {"model1" : 357 , "model2" : 12, "model3" : 75 },
+	"Toyota" : {"Tmodel1" : 850 , "Tmodel2" : 55 , "Tmodel3" : 89}
+}
+
+def main():
+	
+
+def if __name__ == '__main__':
+
+	main()
+	
+
+from datetime import datetime
+
+class rentingOrder :
+	
+	def __init__ (self ,requestDate, generatedID, make, model, Mode)
+
+	self.status = "Not available"
+	self.requestDate = datetime.now()
+	self.returnDate = Null
+	self.rate = 0.00
+	self.availableStock =  currentStock
+	self.id = generatedID
+	self.model = model
+	self.make = make
+	self.rentingMode = Mode
+
+
+
+	def __Return__():
+		set self.status = "available"
+		set self.returnDate = datetime.now()
+		period = self.returnDate - self.requestDate
+		price = self.rate * period
+		bill = print (" youhave rented the car for " + f"{period} " + "amount of time"
+			print (" your total is " + f"{price} ") )
+
+		return bill
+
+
+	def __display__ ():
+
+		show = print (stock)
+
+
